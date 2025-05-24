279c192dd966cd984559d4c042aae8783663725e
WAsbry <WAsbry@163.com>
2025-05-24 19:05:53 +0800

    新建任务页：1.修改页面顶部栏提供返回图标，2.提供Tab 控件，点击后动态更改页面内容

--- app/src/main/java/com/wasbry/nextthing/MainActivity.kt
+++ app/src/main/java/com/wasbry/nextthing/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.wasbry.nextthing
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import com.wasbry.nextthing.navigation.NavigationGraph
 import com.wasbry.nextthing.ui.theme.NextThingTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
