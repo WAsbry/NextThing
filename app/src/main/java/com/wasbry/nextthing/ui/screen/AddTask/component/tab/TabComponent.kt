e4f3d16fbb01bde2740914100ba7fd860b160b8f
WAsbry <WAsbry@163.com>
2025-05-25 01:54:12 +0800

    新建任务页：1.新增健身、工作、生活、娱乐四个分类2.点击后将展示对应分类的图标资源

--- app/src/main/java/com/wasbry/nextthing/ui/screen/AddTask/component/tab/TabComponent.kt
+++ app/src/main/java/com/wasbry/nextthing/ui/screen/AddTask/component/tab/TabComponent.kt
@@ -1,4 +1,4 @@
-package com.wasbry.nextthing.ui.screen.AddTask.component
+package com.wasbry.nextthing.ui.screen.AddTask.component.tab
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -7,23 +7,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
